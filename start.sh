2ec1a86f9e4e6621b7dd5e3c765a80111bc26920
rafisepuh1 <148746292+rafisepuh1@users.noreply.github.com>
2023-10-23 14:16:03 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/rafisepuh1/miror ok
+cd ok
+pip3 install -U -r requirements.txt
+python3 -m bot
